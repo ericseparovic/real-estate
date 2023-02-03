e701dbcaac52aa9bac9e6794eab204c94677b89e
Eric Separovic <ericseparovic07@gmail.com>
2023-02-02 21:46:06 -0300

    Install and Config pug

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express();
 //Routing
 app.get("/", userRouters);
 
+//Enable pug
+app.set("view engine", "pug");
+app.set("views", "./views");
+
 //Define port and start the app\
 const port = 3000;
 app.listen(port, () => {
