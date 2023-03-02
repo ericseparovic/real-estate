f8b229fe1e575f126dde32002e0252279709cd3d
Eric Separovic <ericseparovic07@gmail.com>
2023-03-02 11:01:44 -0300

    Add config db

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import userRouters from './routes/userRouters.js';
+import db from './config/db.js';
 
 // Create app
 const app = express();
+
+// Conection db
+try {
+	await db.authenticate();
+	console.log('Conection ok of database');
+} catch (error) {
+	console.log(error);
+}
 
 // Enable pug
 app.set('view engine', 'pug');
